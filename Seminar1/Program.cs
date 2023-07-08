2c64cd34af56439dce95d00a471f7ff852703efe
Sergey <barabashka089@gmail.com>
2023-07-08 21:34:19 +0300

    Первый семинар

---
+++ Seminar1/Program.cs
@@ -0,0 +1,46 @@
+﻿// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n+5);
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n*n);
+
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// if (n/n1 == n1) 
+//     Console.WriteLine("yes");
+// else 
+//     Console.WriteLine("no");
+
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n==1) Console.WriteLine("Понедельник");
+// else if (n==2) Console.WriteLine("Вторник");
+// else if (n==3) Console.WriteLine("Среда");
+// else if (n==4) Console.WriteLine("Четверг");
+// else if (n==5) Console.WriteLine("Пятница");
+// else if (n==6) Console.WriteLine("Суббота");
+// else if (n==7) Console.WriteLine("Воскресение");
+// else  Console.WriteLine("Вы ввели неверное число");
+
+
+// Console.Clear();
+//  Console.WriteLine("Введите число: ");
+//   int n = Convert.ToInt32(Console.ReadLine());
+//   int a = -n;
+//   while (a<=n) 
+//   {
+//     Console.Write($"{a}, ");
+//     a++;
+//   }
+
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Последнее число: {a%10}");
